844df3ba5880efa0bf92f77c47ee050d304e8b8c
Alzound <61034726+Alzound@users.noreply.github.com>
2023-10-26 15:40:14 -0600

    cambio de variables

--- PR - Primer proyecto/Assets/LobbyManager.cs
+++ PR - Primer proyecto/Assets/LobbyManager.cs
@@ -1,66 +1,3 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
-
-public class LobbyManager : MonoBehaviour
-{
-    public List<GameObject> joinPosManager;
-    public InputAction join1;
-    public InputAction join2;
-    public InputAction startGame;
-
-    public GameObject pos; 
-
-
-    public int Tindex = 0; 
-    //public InputAction action;
-    public List<int> players = new List<int>();
-    // Start is called before the first frame update
-    void Start()
-    {
-        join1.Enable();
-       // join2.Enable();
-        join1.performed += (call) => JoinPlayer(call, 0);
-        //join2.performed += (call) => JoinPlayer(call, 1);
-
-        
-    }
-
-    void JoinPlayer(InputAction.CallbackContext callback, int index)
-    {
-        
-        if (players.Contains(index)) return;
-        Debug.Log(index); 
-        var input = PlayerInputManager.instance.JoinPlayer();
-
-        string scheme = "Keyboard&Mouse";
-        if (index == 1) scheme = "Keyboard2";
-
-        PlayerInput.all[input.playerIndex].SwitchCurrentControlScheme(controlScheme: scheme, Keyboard.current);
-
-        players.Add(index);
-        join1.Disable();
-        //join2.Disable(); 
-    }
-
-    private void Update()
-    {
-        
-    }
-    public void OnJoinPlayer(PlayerInput input)
-    {
-
-        Transform correctPos = pos.gameObject.transform;//joinPosManager[Tindex].gameObject.transform;
-        input.transform.SetPositionAndRotation(correctPos.position, correctPos.rotation);
-        Debug.Log("Me he unido2");
-    }
-
-    
-}
-
-*/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -136,7 +73,7 @@ public class LobbyManager : MonoBehaviour
     {
         timer++; 
         Debug.Log("Quedan: " + timer);
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(8);
         SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex + 1);
         Debug.Log("yes"); 
     }
--- PR - Primer proyecto/Assets/Scripts/Tank.cs
+++ PR - Primer proyecto/Assets/Scripts/Tank.cs
@@ -5,29 +5,53 @@ using UnityEngine.InputSystem;
 
 public class Tank : MonoBehaviour
 {
+    [Header("Input system")]
+    private PlayerInput playerInput;
+    private InputAction move, quit, fire;
 
+    [SerializeField]
+    [Header("Tank variables")]
     public Vector3 spawnPos;
-    public InputAction move;
+    //public InputAction move;
     Vector2 tankMovSpeed;
     float tankSpeed = 15f;
     float tankRot = 510f;
+    public bool isMoving; 
 
- 
-    public GameObject proyectile;
+    [Header("Tank projectiles")]
+    public GameObject projectile;
     public int poolCapacity = 5;
     public List<GameObject> bullets;
     public GameObject spawnBPoint;
     public GameObject bullet;
     public Transform bulletSpawnpref;
+    private int currentBulletIndex = 0;
 
-
+    [Header("Tank materials")]
     public List<Material> mat;
     public GameObject chasis;
     public GameObject top;
     public int num = 0;
 
-    public int tankInd = 0; 
-  
+    public int tankInd = 0;
+
+
+    private void OnEnable()
+    {
+        move.performed += Movement;
+        fire.performed += Fire; 
+        quit.performed += Quit;
+        quit.canceled += Quit;
+    }
+
+    private void OnDisable()
+    {
+        move.performed -= Movement;
+        fire.performed -= Fire;
+        quit.performed -= Quit;
+        quit.canceled -= Quit;
+    }
+
 
     private void Start()
     {
@@ -39,25 +63,22 @@ public class Tank : MonoBehaviour
         top.GetComponent<Renderer>().material = mat[num];
         for (int i = 0; i < poolCapacity; i++)
         {
-            GameObject obj = (GameObject)Instantiate(proyectile);
+            GameObject obj = (GameObject)Instantiate(projectile);
             obj.SetActive(false);
             bullets.Add(obj);
         }
-        
-    }
 
+        currentBulletIndex = 0; // Inicializa el índice de la bala actual
+    }
 
     private void Update()
     {
         transform.Translate(tankSpeed * tankMovSpeed.y * Time.deltaTime * Vector3.forward);
         transform.Rotate(tankRot * tankMovSpeed.x * Time.deltaTime * Vector3.up);
-      //  change();
-
     }
 
     public void SetInitialPos(Vector3 position)
     {
-        
         spawnPos = position;
         transform.position = position;
     }
@@ -69,23 +90,31 @@ public class Tank : MonoBehaviour
 
     public void Fire(InputAction.CallbackContext context)
     {
-        if (context.phase != InputActionPhase.Started) return;
-
-        for (int i = 0; i < bullets.Count; i++)
+        if (context.performed)
         {
-            if(!bullets[i].activeInHierarchy)
+            if (currentBulletIndex < bullets.Count)
             {
-                bullets[i].transform.position = spawnBPoint.transform.position;
-                bullets[i].transform.rotation = spawnBPoint.transform.rotation;
-                bullets[i].SetActive(true);
-                bullets[i].GetComponent<Rigidbody>().AddForce(bulletSpawnpref.forward * 1000 * 2);
-                break;
+                GameObject currentBullet = bullets[currentBulletIndex];
+
+                if (!currentBullet.activeInHierarchy)
+                {
+                    currentBullet.transform.position = spawnBPoint.transform.position;
+                    currentBullet.transform.rotation = spawnBPoint.transform.rotation;
+                    currentBullet.SetActive(true);
+                    currentBullet.GetComponent<Rigidbody>().AddForce(bulletSpawnpref.forward * 1000 * 2);
+                    currentBulletIndex++; // Avanza al siguiente índice de bala
+                }
             }
         }
     }
 
-    public void Move(InputAction.CallbackContext context)
+    public void Movement(InputAction.CallbackContext context)
     {
         tankMovSpeed = context.ReadValue<Vector2>();
+        isMoving = true; 
+    }
+    public void Quit(InputAction.CallbackContext context)
+    {
+        Application.Quit();
     }
 }
